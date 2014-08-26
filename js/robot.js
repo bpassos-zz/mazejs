b2c477faa9875914ef51cba39c5bb6478cc1419d
Bruno Passos <bruno.passos@booking.com>
2014-08-26 17:21:39 +0200

    Added some robot features

---
+++ js/robot.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function Robot () {
+    this.x = null;
+    this.y = null;
+    this.orientation = null;
+    this.maze = null;
+}
+
+Robot.prototype.setMaze = function() {
+    this.maze = maze;
+}
